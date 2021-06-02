ec8dbf7759f7255d27d33244fa140022577ec75c
jeffery2999 <44108351+jeffery2999@users.noreply.github.com>
2021-06-02 21:50:12 +0800

    Update swimming_poolTest.java

--- app/src/test/java/demo/swimming_poolTest.java
+++ app/src/test/java/demo/swimming_poolTest.java
@@ -40,128 +40,128 @@ class swimming_poolTest {
 		assertEquals(result, Test.price(member, weekend, group, age, time));
 	}
 	
-	@Test
-	void test_02() throws Exception {
-		String result = "킠챞턨짫!";
-		boolean member = true;
-		boolean weekend = true;
-		boolean group = false;
-		int age = 2;
-		LocalTime time = LocalTime.parse("09:00:00");
-		Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
-		assertEquals(result, exception.getMessage());
-	}
+	// @Test
+	// void test_02() throws Exception {
+	// 	String result = "킠챞턨짫!";
+	// 	boolean member = true;
+	// 	boolean weekend = true;
+	// 	boolean group = false;
+	// 	int age = 2;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
+	// 	assertEquals(result, exception.getMessage());
+	// }
 	
-	@Test
-	void test_03() throws Exception {
-		String result = "킠챞턨짫!";
-		boolean member = true;
-		boolean weekend = true;
-		boolean group = false;
-		int age = 3;
-		LocalTime time = LocalTime.parse("04:59:00");
-		Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
-		assertEquals(result, exception.getMessage());
-	}
+	// @Test
+	// void test_03() throws Exception {
+	// 	String result = "킠챞턨짫!";
+	// 	boolean member = true;
+	// 	boolean weekend = true;
+	// 	boolean group = false;
+	// 	int age = 3;
+	// 	LocalTime time = LocalTime.parse("04:59:00");
+	// 	Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
+	// 	assertEquals(result, exception.getMessage());
+	// }
 	
-	@Test
-	void test_04() throws Exception {
-		double result = 250;
-		boolean member = false;
-		boolean weekend = true;
-		boolean group = false;
-		int age = 3;
-		LocalTime time = LocalTime.parse("09:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}
+	// @Test
+	// void test_04() throws Exception {
+	// 	double result = 250;
+	// 	boolean member = false;
+	// 	boolean weekend = true;
+	// 	boolean group = false;
+	// 	int age = 3;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }
 	
-	@Test
-	void test_05() throws Exception {
-		double result = 100;
-		boolean member = true;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 3;
-		LocalTime time = LocalTime.parse("09:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}
+	// @Test
+	// void test_05() throws Exception {
+	// 	double result = 100;
+	// 	boolean member = true;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 3;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }
 	
-	@Test
-	void test_06() throws Exception {
-		double result = 140;
-		boolean member = false;
-		boolean weekend = false;
-		boolean group = true;
-		int age = 3;
-		LocalTime time = LocalTime.parse("09:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}
+	// @Test
+	// void test_06() throws Exception {
+	// 	double result = 140;
+	// 	boolean member = false;
+	// 	boolean weekend = false;
+	// 	boolean group = true;
+	// 	int age = 3;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }
 	
-	@Test
-	void test_07() throws Exception {
-		String result = "킠챞턨짫!";
-		boolean member = true;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 2;
-		LocalTime time = LocalTime.parse("09:00:00");
-		Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
-		assertEquals(result, exception.getMessage());
-	}
+	// @Test
+	// void test_07() throws Exception {
+	// 	String result = "킠챞턨짫!";
+	// 	boolean member = true;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 2;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
+	// 	assertEquals(result, exception.getMessage());
+	// }
 	
-	@Test
-	void test_08() throws Exception {
-		String result = "킠챞턨짫!";
-		boolean member = true;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 3;
-		LocalTime time = LocalTime.parse("04:59:00");
-		Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
-		assertEquals(result, exception.getMessage());
-	}
+	// @Test
+	// void test_08() throws Exception {
+	// 	String result = "킠챞턨짫!";
+	// 	boolean member = true;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 3;
+	// 	LocalTime time = LocalTime.parse("04:59:00");
+	// 	Exception exception = assertThrows(Exception.class, () -> Test.price(member, weekend, group, age, time));
+	// 	assertEquals(result, exception.getMessage());
+	// }
 
-	@Test
-	void test_09() throws Exception {
-		double result = 160;
-		boolean member = false;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 61;
-		LocalTime time = LocalTime.parse("09:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}
+	// @Test
+	// void test_09() throws Exception {
+	// 	double result = 160;
+	// 	boolean member = false;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 61;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }
 
-	@Test
-	void test_10() throws Exception {
-		double result = 200;
-		boolean member = false;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 25;
-		LocalTime time = LocalTime.parse("09:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}
+	// @Test
+	// void test_10() throws Exception {
+	// 	double result = 200;
+	// 	boolean member = false;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 25;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }
 
-	@Test
-	void test_11() throws Exception {
-		double result = 160;
-		boolean member = false;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 25;
-		LocalTime time = LocalTime.parse("06:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}	
+	// @Test
+	// void test_11() throws Exception {
+	// 	double result = 160;
+	// 	boolean member = false;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 25;
+	// 	LocalTime time = LocalTime.parse("06:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }	
 	
-	@Test
-	void test_12() throws Exception {
-		double result = 200;
-		boolean member = false;
-		boolean weekend = false;
-		boolean group = false;
-		int age = 25;
-		LocalTime time = LocalTime.parse("09:00:00");
-		assertEquals(result, Test.price(member, weekend, group, age, time));
-	}	
+	// @Test
+	// void test_12() throws Exception {
+	// 	double result = 200;
+	// 	boolean member = false;
+	// 	boolean weekend = false;
+	// 	boolean group = false;
+	// 	int age = 25;
+	// 	LocalTime time = LocalTime.parse("09:00:00");
+	// 	assertEquals(result, Test.price(member, weekend, group, age, time));
+	// }	
 }
